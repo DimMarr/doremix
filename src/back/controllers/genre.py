e84e91b99d3294fe03109a513e70194d34c436a9
Charley Bignon <charley.bignon@etu.umontpellier.fr>
2026-01-16 12:26:27 +0100

    refactor: remove unused genre retrieval methods from GenreController and GenreRepository

--- src/back/controllers/genre.py
+++ src/back/controllers/genre.py
@@ -7,10 +7,3 @@ class GenreController:
     @staticmethod
     def get_all_genres(db: Session):
         return GenreRepository.get_all(db)
-
-    @staticmethod
-    def get_genre(db: Session, genre_id: int):
-        genre = GenreRepository.get_by_id(db, genre_id)
-        if not genre:
-            raise HTTPException(status_code=404, detail="Genre not found")
-        return genre
--- src/back/repositories/genre_repository.py
+++ src/back/repositories/genre_repository.py
@@ -7,7 +7,3 @@ class GenreRepository:
     @staticmethod
     def get_all(db: Session) -> List[Genre]:
         return db.query(Genre).all()
-
-    @staticmethod
-    def get_by_id(db: Session, genre_id: int) -> Optional[Genre]:
-        return db.query(Genre).filter(Genre.idGenre == genre_id).first()
