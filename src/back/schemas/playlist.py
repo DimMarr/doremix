c340235775a553cc45a2eb3e7a6cd3e45ebda260
Ilias Ouchman <ilias.ouchman@etu.umontpellier.fr>
2026-01-16 12:01:06 +0100

    fix: warning test playlist

--- src/back/schemas/playlist.py
+++ src/back/schemas/playlist.py
@@ -22,8 +22,7 @@ class PlaylistSchema(BaseModel):
     createdAt: datetime
     updatedAt: datetime
 
-    class Config:
-        from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
 
 
 class PlaylistCreate(BaseModel):
