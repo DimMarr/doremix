903c50839cf610c0e19eb52ba80d04bcef0b7a6e
ghadi.saab <ghadi.saab@etu.umontpellier.fr>
2026-01-23 13:21:00 +0100

    test: added backend tests to test the searchbar, playlist creation and upload of image.

--- src/back/tests/conftest.py
+++ src/back/tests/conftest.py
@@ -7,6 +7,7 @@ from fastapi.testclient import TestClient
 from database import Base, get_db
 from routes.playlists import router as playlists_router
 from routes.users import router as users_router
+from routes.search_router import router as search_router
 
 # Crée une base de données SQLite en mémoire pour les tests
 SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
@@ -22,6 +23,7 @@ TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=test_
 app = FastAPI()
 app.include_router(playlists_router)
 app.include_router(users_router)
+app.include_router(search_router)
 
 
 @pytest.fixture(scope="function")
