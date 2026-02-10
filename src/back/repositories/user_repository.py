10d7ae009f9e1a5bc9bdfb4bcf07445a7d7f8b0d
nadia.lahya <nadia.lahya@etu.umontpellier.fr>
2026-02-10 20:26:45 +0100

    fix: pre-commit fix for user repository

--- src/back/repositories/user_repository.py
+++ src/back/repositories/user_repository.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import or_
 from models.user import User, UserRole
 
+
 class UserRepository:
     @staticmethod
     def create(db: Session, user: User) -> User:
@@ -14,31 +15,33 @@ class UserRepository:
     def get_all(db: Session):
         users = db.query(User).all()
         return users
-    
+
     @staticmethod
     def get_user_by_id(db: Session, user_id: int):
-            return db.query(User).filter(User.idUser == user_id).first()
+        return db.query(User).filter(User.idUser == user_id).first()
 
     @staticmethod
     def get_by_email_or_username(db: Session, email: str, username: str):
-        return db.query(User).filter(
-            or_(User.email == email, User.username == username)
-        ).first()
-    
+        return (
+            db.query(User)
+            .filter(or_(User.email == email, User.username == username))
+            .first()
+        )
+
     @staticmethod
     def create_user(db: Session, email: str, username: str, password_hash: str):
         db_user = User(
             email=email,
             username=username,
-            password=password_hash,  
-            role=UserRole.USER,      # default role
-            banned=False
+            password=password_hash,
+            role=UserRole.USER,  # default role
+            banned=False,
         )
-        
+
         db.add(db_user)
         db.commit()
         db.refresh(db_user)
-        
+
         return db_user
 
     @staticmethod
@@ -55,7 +58,7 @@ class UserRepository:
             .all()
         )
         return users
-    
+
     @staticmethod
     def update_role(db: Session, user_id: int, new_role: UserRole):
         user = db.query(User).filter(User.idUser == user_id).first()
@@ -64,4 +67,3 @@ class UserRepository:
             db.commit()
             db.refresh(user)
         return user
-    
