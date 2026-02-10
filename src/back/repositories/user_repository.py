7e667733193cdab4a6fca89f8ad0edde11105cb9
nadia.lahya <nadia.lahya@etu.umontpellier.fr>
2026-02-10 19:55:11 +0100

    feat: implement user repository and registration schema with validation

---
+++ src/back/repositories/user_repository.py
@@ -0,0 +1,67 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import or_
+from models.user import User, UserRole
+
+class UserRepository:
+    @staticmethod
+    def create(db: Session, user: User) -> User:
+        db.add(user)
+        db.commit()
+        db.refresh(user)
+        return user
+
+    @staticmethod
+    def get_all(db: Session):
+        users = db.query(User).all()
+        return users
+    
+    @staticmethod
+    def get_user_by_id(db: Session, user_id: int):
+            return db.query(User).filter(User.idUser == user_id).first()
+
+    @staticmethod
+    def get_by_email_or_username(db: Session, email: str, username: str):
+        return db.query(User).filter(
+            or_(User.email == email, User.username == username)
+        ).first()
+    
+    @staticmethod
+    def create_user(db: Session, email: str, username: str, password_hash: str):
+        db_user = User(
+            email=email,
+            username=username,
+            password=password_hash,  
+            role=UserRole.USER,      # default role
+            banned=False
+        )
+        
+        db.add(db_user)
+        db.commit()
+        db.refresh(db_user)
+        
+        return db_user
+
+    @staticmethod
+    def search_users(db: Session, query: str, limit: int = 10):
+        users = (
+            db.query(User)
+            .filter(
+                or_(
+                    User.username.ilike(f"%{query}%"),
+                    User.email.ilike(f"%{query}%"),
+                )
+            )
+            .limit(limit)
+            .all()
+        )
+        return users
+    
+    @staticmethod
+    def update_role(db: Session, user_id: int, new_role: UserRole):
+        user = db.query(User).filter(User.idUser == user_id).first()
+        if user:
+            user.role = new_role
+            db.commit()
+            db.refresh(user)
+        return user
+    
--- src/back/schemas/user.py
+++ src/back/schemas/user.py
@@ -1,7 +1,8 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, field_validator
 from enum import Enum
 from typing import Optional, List
 from .playlist import PlaylistSchema
+import re  # for regex operations
 
 
 class UserRole(str, Enum):
@@ -19,3 +20,25 @@ class UserSchema(BaseModel):
     playlists: Optional[List[PlaylistSchema]] = None
 
     model_config = ConfigDict(from_attributes=True)
+
+
+class UserRegisterSchema(BaseModel):
+    email: str
+    username: str
+    password: str
+
+    @field_validator("email")
+    def validate_email(cls, email):
+        if not re.match(
+            r"^[a-zA-Z0-9._-]+@(etu\.)?umontpellier\.fr$", email
+        ):  # name.lastname@[et].umontpellier.fr
+            raise ValueError("Invalid email format")
+        return email
+
+    @field_validator("username")
+    def validate_username(cls, username):
+        if not re.match(r"^[a-zA-Z0-9_.]+$", username):  # name.lastname
+            raise ValueError(
+                "Username can only contain letters, numbers, and underscores"
+            )
+        return username
