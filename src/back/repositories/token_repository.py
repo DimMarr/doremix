f38e7760acab5b09c931204301cdf59e5fcb5450
nadia.lahya <nadia.lahya@etu.umontpellier.fr>
2026-02-10 22:21:11 +0100

    fix :  import errors in pre-commit

--- src/back/repositories/token_repository.py
+++ src/back/repositories/token_repository.py
@@ -3,44 +3,56 @@ import secrets
 import hashlib
 from datetime import datetime, timedelta, timezone
 from sqlalchemy.orm import Session
-from models.token import Token, TokenType
+from models.token import Token, TokenTypes
+from typing import cast
+
 
 class TokenRepository:
-
     @staticmethod
     def hash_token(cookieToken: str) -> str:
         secret_key = os.getenv("TOKEN_SECRET_KEY")
         if not secret_key:
-             raise ValueError("TOKEN_SECRET_KEY is missing in .env file")
+            raise ValueError("TOKEN_SECRET_KEY is missing in .env file")
 
         combined = cookieToken + secret_key
         return hashlib.sha256(combined.encode("utf-8")).hexdigest()
 
     @staticmethod
-    def create_token(db: Session, userId: int, token_type: TokenType, durationMinutes: int) -> Token:
-        cookieToken = secrets.token_urlsafe(64) # not stored in DB
+    def create_token(
+        db: Session, userId: int, token_type: TokenTypes, durationMinutes: int
+    ) -> Token:
+        cookieToken = secrets.token_urlsafe(64)  # not stored in DB
         hashedToken = TokenRepository.hash_token(cookieToken)
         expiresAt = datetime.now(timezone.utc) + timedelta(minutes=durationMinutes)
 
-        dbToken = Token(token=hashedToken, type=token_type, idUser=userId, expiresAt=expiresAt)
-        
+        dbToken = Token(
+            token=hashedToken, type=token_type, idUser=userId, expiresAt=expiresAt
+        )
+
         db.add(dbToken)
         db.commit()
         db.refresh(dbToken)
-        
+
         # we return the unhashed token to use it in cookies
-        dbToken.token = cookieToken 
+        dbToken.token = cookieToken
         return dbToken
 
     @staticmethod
-    def get_valid_token(db: Session, cookieTokenStr: str, required_type: TokenType) -> Token | None:
+    def get_valid_token(
+        db: Session, cookieTokenStr: str, required_type: TokenTypes
+    ) -> Token | None:
         hashedTokenToCheck = TokenRepository.hash_token(cookieTokenStr)
 
-        return db.query(Token).filter(
-            Token.token == hashedTokenToCheck,
-            Token.type == required_type,
-            Token.expiresAt > datetime.now(timezone.utc)
-        ).first()
+        result = (
+            db.query(Token)
+            .filter(
+                Token.token == hashedTokenToCheck,
+                Token.type == required_type,
+                Token.expiresAt > datetime.now(timezone.utc),
+            )
+            .first()
+        )
+        return cast(Token, result) if result else None
 
     @staticmethod
     def revoke_token(db: Session, cookieTokenStr: str):
