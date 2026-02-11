86e7fbc6e3c2f92355aca6ef628ee0ebe9df88a9
nadia.lahya <nadia.lahya@etu.umontpellier.fr>
2026-02-11 22:58:22 +0100

    fix : pipeline failure

--- src/back/routes/register.py
+++ src/back/routes/register.py
@@ -7,11 +7,12 @@ from schemas.user import UserRegisterSchema
 # /auth/register, /auth/login...
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
+
 @router.post(
     "/register",
     status_code=status.HTTP_201_CREATED,
     summary="Créer un nouveau compte",
-    description="Enregistre un utilisateur, hache le mot de passe et génère un username unique."
+    description="Enregistre un utilisateur, hache le mot de passe et génère un username unique.",
 )
 def register(user_data: UserRegisterSchema, db: Session = Depends(get_db)):
     return RegisterController.register(db, user_data)
