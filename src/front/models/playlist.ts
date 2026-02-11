a69f8181c7fbc0c2ffd61373261a63a6b45226df
Dimitri Martel <dimitri.martel@etu.umontpellier.fr>
2026-02-11 16:57:46 +0100

    feat: Display categorized personal and public playlists on the home page with a redesigned card component and updated playlist model.

--- src/front/models/playlist.ts
+++ src/front/models/playlist.ts
@@ -2,7 +2,7 @@ import { Track } from "@models/track";
 
 export default class Playlist {
     public idPlaylist?: number;
-    private idOwner?: number;
+    public idOwner?: number;
     public name?: string;
     private idGenre?: number;
     public createdAt?: string;
