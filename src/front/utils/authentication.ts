d56afa9e05548fec7c50bf45b57da301b6c9a259
Jessy Latmi <jessy.latmi@etu.umontpellier.fr>
2026-02-16 14:05:15 +0100

    test: cookies auth

--- src/front/utils/authentication.ts
+++ src/front/utils/authentication.ts
@@ -1,4 +1,4 @@
-import { error } from "console";
+// import { error } from "console";
 import { API_BASE_URL } from "@config/index";
 
 export function isValidPassword(password: string): boolean {
@@ -12,104 +12,80 @@ export function isValidEmail(email: string): boolean {
 }
 
 class AuthService {
-    /*
-     * La classe AuthService contient un attribut accessToken
-     * Si l'utilisateur a un accessToken, on vérifie la validité
-     * Si le token est valide, on est authentifié. Sinon, si on a un cookie refreshToken, on essaie de générer un nouveau accessToken.
-     * Si on a réussi à le générer, on est authentifié. Sinon, on est renvoyé vers login.
-     */
-
-    accessToken: string | null;
-    constructor() {
-        this.accessToken = null;
-    }
-
     async isAuthenticated(){
-        const hasRefreshToken = document.cookie.includes("refreshToken")
+        // const hasRefreshToken = document.cookie.includes("refreshToken")
+        // const hasAccessToken = document.cookie.includes("accessToken")
 
-        // Check if accessToken is still valid
-        // if yes, return true (isAuthenticated)
-        // if no, set this.accessToken = null
-        // try{
-        //     const response = await fetch(`${API_BASE_URL}/auth/checkAccessToken`, {
-        //         method: 'POST',
-        //         credentials: 'same-origin'
-        //     })
-
-        //     const data = await response.json();
-        //     if (data.valid == false) {
-        //         this.accessToken = null;
-        //     } else {
-        //         return true
-        //     }
-        // } catch (e) {
-        //     console.error('Cannot check accessToken validity', e);
-        //     throw error;
+        // if (!hasAccessToken){
+        //     return false
         // }
 
-        // If no accessToken, try to get a new one
-        if(this.accessToken == null && hasRefreshToken){
-            console.log("Calling for a new refreshAccessToken")
-            await this.refreshAccessToken();
+        try{
+            const response = await fetch(`${API_BASE_URL}/auth/check-token`, {
+                method: 'POST',
+                credentials: 'same-origin'
+            })
+            if (response.status == 200) {
+                return true
+            }
+        } catch {
+            const refresh = await this.refreshAccessToken()
+            console.log(`refresh: ${refresh}`)
+            if (refresh){
+                return true
+            }
+            return false
         }
-        console.log("accessToken :", this.accessToken)
-
-        return this.accessToken != null;
     }
 
     // Login provides a new accessToken
     async login(email, password){
-        // try{
-        //     const response = await fetch(`${API_BASE_URL}/auth/login`, {
-        //         method: 'POST',
-        //         headers: { 'Content-Type': 'application/json'},
-        //         credentials: 'same-origin', // or include ?
-        //         body: JSON.stringify({email, password})
-        //     })
+        try{
+            const response = await fetch(`${API_BASE_URL}/auth/login`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json'},
+                credentials: 'include',
+                body: JSON.stringify({
+                    "email": email,
+                    "password": password
+                })
+            })
 
-        //     const data = await response.json();
-        //     this.accessToken = data.accessToken;
-        // } catch (e) {
-        //     console.error('Login failed', e);
-        //     throw error;
-        // }
-
-        document.cookie = `refreshToken=${Math.random().toString(36).substring(2, 2 + 16)}`;
-        this.accessToken = Math.random().toString(36).substring(2, 2 + 6);;
+            const result = await response.json()
+            console.log(result)
+            // document.cookie = `accessToken=${result.access_token}; path=/`
+            // document.cookie = `refreshToken=${result.refresh_token}; path=/`
+        } catch (e) {
+            console.log('Login failed', e);
+            throw error;
+        }
     }
 
     // refresh accessToken through refreshToken
     async refreshAccessToken() {
-        // try {
-        //     const response = await fetch(`${API_BASE_URL}/auth/refresh`, {
-        //         method: 'POST',
-        //         credentials: 'same-origin' // or include ?
-        //     })
-
-        //     const data = await response.json()
-        //     this.accessToken = data.accessToken;
-        // } catch(e) {
-        //     console.error('Token refresh failed', e);
-        //     throw error;
-        // }
+        const response = await fetch(`${API_BASE_URL}/auth/refresh`, {
+            method: 'POST',
+            credentials: 'same-origin' // or include ?
+        })
+        return response.status == 200
 
         // Trying to replicate success without backend
-        if(document.cookie.includes("refreshToken")){
-            this.accessToken = Math.random().toString(36).substring(2, 2 + 6);
-            console.log("accessToken provided.")
-            return
-        }
-        console.log("Do not have a refreshToken.")
+        // if(document.cookie.includes("refreshToken")){
+        //     this.accessToken = Math.random().toString(36).substring(2, 2 + 6);
+        //     console.log("accessToken provided.")
+        //     return
+        // }
+        // console.log("Do not have a refreshToken.")
 
     }
 
     async logout() {
-        this.accessToken = null;
-        document.cookie = "refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
-        // const response = await fetch(`${API_BASE_URL}/auth/logout`, {
-        //     method: 'POST',
-        //     credentials: 'same-origin' // or include ?
-        // })
+        // this.accessToken = null;
+        // document.cookie = "refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
+        const response = await fetch(`${API_BASE_URL}/auth/logout`, {
+            method: 'POST',
+            credentials: 'same-origin' // or include ?
+        })
     }
 }
 
