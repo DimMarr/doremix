6d0c8f488a14483622c492ba1ad5b7135657462a
Jessy Latmi <jessy.latmi@etu.umontpellier.fr>
2026-03-20 12:14:43 +0100

    feat: added user model on frontend

--- src/front/models/index.ts
+++ src/front/models/index.ts
@@ -1,3 +1,5 @@
 export { Artist } from '@models/artist';
 export { Track } from '@models/track';
+export { default as User, Role } from '@models/user';
+export { Genre } from '@models/genre';
 export { default as Playlist, Visibility } from '@models/playlist';
