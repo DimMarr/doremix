b0b99151ee8beef0cc8dd4254c8f3d94b43929e6
nadia.lahya <nadia.lahya@etu.umontpellier.fr>
2026-04-01 21:56:09 +0200

    fix: footer

--- src/front/components/generics/footer.tsx
+++ src/front/components/generics/footer.tsx
@@ -3,9 +3,8 @@ export function Footer() {
     <footer class="bg-background rounded-md p-4 mt-12 mb-3">
       <div class="flex flex-col md:flex-row gap-6 justify-between mb-4">
 
-        {/* --- Section Gauche --- */}
         <div class="company-section max-w-[350px]">
-          <div class="font-black text-xl mb-1">Dorémix</div>
+          <div class="font-black text-xl mb-1">Doremix</div>
           <p class="mb-2 text-sm text-neutral-300">Your all in one solution to listen to music.</p>
           <div class="inline-flex items-center justify-center rounded-full border px-2 py-0.5 text-[10px] font-medium w-fit whitespace-nowrap shrink-0 gap-1 focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden border-transparent bg-neutral-800 text-secondary-foreground">
             <span class="w-[4px] h-[4px] rounded-[999px] bg-green-500 mr-1"></span>
