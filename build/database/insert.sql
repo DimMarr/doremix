29777f09fbe0bc5f2e63ae628367a6033ae0eda6
dimitri.martel <dimitri.martel@etu.umontpellier.fr>
2026-04-01 07:37:46 +0000

    feat: Implement drag-and-drop track reordering for playlists using a linked-list

--- build/database/insert.sql
+++ build/database/insert.sql
@@ -116,6 +116,8 @@ INSERT INTO PLAYLIST (name, idGenre, idOwner, visibility, vote, coverImage) VALU
 
 INSERT INTO TRACK_PLAYLIST (idTrack, idPlaylist, nameInPlaylist, next_track_id) VALUES
 (2, 1, NULL, 3),
+(2, 1, NULL, 18),
+(18, 1, 'Inception (Best OST)', 3),
 (3, 1, NULL, 4),
 (4, 1, NULL, 5),
 (5, 1, NULL, NULL),
