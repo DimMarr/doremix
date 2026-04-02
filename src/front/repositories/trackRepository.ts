ec198bdba58eaa1c957c3419ec47e5add0b04cf4
kilian.nagel <kilian.nagel@etu.umontpellier.fr>
2026-04-02 12:42:39 +0200

    feat: share playlist with a group in CLI

--- src/front/repositories/trackRepository.ts
+++ src/front/repositories/trackRepository.ts
@@ -109,37 +109,4 @@ export class TrackRepository {
       throw err;
     }
   }
-  
-  async shareGroup(playlistId: number, groupName: string) {
-    const response = await fetch(`${API_BASE_URL}/playlists/${playlistId}/share/group`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ "group_name": groupName }),
-      credentials: 'include'
-    });
-    if (!response.ok) {
-      throw new Error("Failed to share with group");
-    }
-    return response.json();
-  }
-  
-  async move(playlistId: number, trackId: number, prev_track_id: number | null) {
-    try {
-      const response = await fetch(`${API_BASE_URL}/playlists/${playlistId}/tracks/${trackId}/move`, {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ prev_track_id }),
-        credentials: 'include'
-      });
-
-      if (!response.ok) {
-        throw new Error("Failed to move track");
-      }
-      return response.json();
-    } catch (err) {
-      throw err;
-    }
-  }
 }
